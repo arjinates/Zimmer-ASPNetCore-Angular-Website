6273ffb84627e21f0e50f7861982370612db40ca
Arjin Ateş <arjinates@hotmail.com>
2023-05-24 12:33:40 +0300

    Basket IRepo and Basket Repo added

---
+++ Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basketId);
+        Task<bool> DeleteBasketAsync(string basketId);
+
+        
+    }
+}
