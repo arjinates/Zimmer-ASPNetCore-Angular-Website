4960bdf72aded9c38402690fcdbe8d7a4aaf88b7
Arjin Ateş <arjinates@hotmail.com>
2023-05-25 19:45:33 +0300

    removeItemFromBasket & deleteBasket methods added

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -39,18 +39,40 @@ export class BasketService {
     return this.basketSource.value;
   }
 
-  addItemToBasket(item: Product, quantity = 1) {
-    const itemToAdd = this.mapProductItemToBasketItem(item);
+  addItemToBasket(item: Product | BasketItem, quantity = 1) {
+    if (this.isProduct(item)) item = this.mapProductItemToBasketItem(item);
     const basket = this.getCurrentBasketValue() ?? this.createBasket(); 
-    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    basket.items = this.addOrUpdateItem(basket.items, item, quantity);
     this.setBasket(basket);
   }
-  private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
-    if (!items) {
-      items = [];
+
+  removeItemFromBasket(id: number, quantity = 1) {
+    const basket = this.getCurrentBasketValue();
+    if (!basket) return;
+    const item = basket.items.find(x => x.id === id);
+    if (item) {
+      item.quantity -= 1;
+      if (item.quantity === 0) {
+        basket.items = basket.items.filter(x => x.id !== id)
+      }
+      if (basket.items.length > 0) this.setBasket(basket);
+      else this.deleteBasket(basket);
+    }
   }
+
+  deleteBasket(basket: Basket) {
+    return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe({
+      next: () => {
+        this.basketSource.next(null);
+        this.basketTotalSource.next(null);
+        localStorage.removeItem('basket_id');
+      }
+    })
+  }
+
+  private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
+    this.isItemsEmpty(items);
     const item = items.find(x => x.id === itemToAdd.id);
-    
     if (item) {item.quantity += quantity;}
     else{
       itemToAdd.quantity = quantity;
@@ -83,5 +105,14 @@ export class BasketService {
     const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
     const total = subtotal + shipping;
   this.basketTotalSource.next({shipping, total, subtotal});
+  }
+
+  private isProduct(item: Product | BasketItem): item is Product {
+    return (item as Product).productBrand !== undefined;
+  }
+
+  private isItemsEmpty(items: BasketItem[]) {
+    if (!items) items = [];
+    return items;
   }
 }
