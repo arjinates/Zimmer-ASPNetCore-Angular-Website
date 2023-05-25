22dd48d61420f5f9abad1391f709a5abbf192ef4
Arjin Ateş <arjinates@hotmail.com>
2023-05-25 20:57:06 +0300

    basket component functions added

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BasketService } from './basket.service';
+import { BasketItem } from '../shared/Models/basket';
 
 @Component({
   selector: 'app-basket',
@@ -8,7 +9,13 @@ import { BasketService } from './basket.service';
 })
 export class BasketComponent {
  
-  constructor(public basketService: BasketService){
-    
+  constructor(public basketService: BasketService){}
+
+  incrementQuantity(item: BasketItem) {
+    this.basketService.addItemToBasket(item);
+  }
+
+  removeItem(id: number, quantity: number) {
+    this.basketService.removeItemFromBasket(id, quantity);
   }
 }
