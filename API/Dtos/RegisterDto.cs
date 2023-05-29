bc480828b466f28a640980e27302c91d7055130f
Arjin Ateş <arjinates@hotmail.com>
2023-05-29 08:28:41 +0300

    register validation added

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto 
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", 
+        ErrorMessage = "Password must have contain at least one letter, one number and be longer than six characters!")]
         public string Password { get; set; }
     }
 }
