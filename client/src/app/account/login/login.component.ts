f5b7903ff21ac63d7bcfb8050ec48a5cb14eeff6
Arjin Ateş <arjinates@hotmail.com>
2023-05-30 23:37:25 +0300

    login component carries reactive forms now

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  }) 
+  
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
