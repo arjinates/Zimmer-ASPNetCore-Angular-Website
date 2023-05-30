83cc82932e4ce401ed16b2140b6c863cead7fab5
Arjin Ateş <arjinates@hotmail.com>
2023-05-30 23:04:22 +0300

    user interface created

---
+++ client/src/app/shared/Models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    email: string
+    dipslayName: string;
+    token: string;
+}
+
+export interface Address {
+    firstName: string
+    lastName: string;
+    city: string;
+    state: string;
+    zipcode: string;
+}
