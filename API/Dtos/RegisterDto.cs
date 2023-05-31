2c8876213cf12f1e3c184b258deefae00bf88177
Arjin Ateş <arjinates@hotmail.com>
2023-06-01 00:12:42 +0300

    regex updated x2

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -10,8 +10,8 @@ namespace API.Dtos
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", 
-        ErrorMessage = "Password must have contain at least one letter, one number and be longer than six characters!")]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+        ErrorMessage = "Password must have contain at least 1 small-case letter, 1 Capital letter, 1 digit, 1 special character and the length should be between 6-10 characters!")]
         public string Password { get; set; }
     }
 }
