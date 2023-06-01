5ccfbb2ad163f7e745c08ab42a9f7d4a95e4ffdc
Arjin Ateş <arjinates@hotmail.com>
2023-06-01 19:31:09 +0300

    parameter classes fixed

--- Infrastructure/Data/Config/DeliveryMethodConfiguration.cs
+++ Infrastructure/Data/Config/DeliveryMethodConfiguration.cs
@@ -1,11 +1,12 @@
 using Core.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Config
 {
     public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
     {
-        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<DeliveryMethod> builder)
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
         {
            builder.Property(d => d.Price)
                 .HasColumnType("decimal(18.2)");
--- Infrastructure/Data/Config/OrderConfiguration.cs
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -1,11 +1,12 @@
 using Core.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Config
 {
     public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
-        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Order> builder)
+        public void Configure(EntityTypeBuilder<Order> builder)
         {
             builder.OwnsOne(o => o.ShipToAddress, a => {
                 a.WithOwner();
