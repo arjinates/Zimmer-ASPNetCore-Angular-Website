2ce9e62a9cf81ec59b389745f5fe83cb42a3d753
Arjin Ateş <arjinates@hotmail.com>
2023-06-01 16:58:17 +0300

    Address and DeliveryMethod aded for Order

---
+++ Core/OrderAggregate/Address.cs
@@ -0,0 +1,26 @@
+namespace Core.OrderAggregate
+{
+    public class Address
+    {
+        public Address()
+        {
+        }
+
+        public Address(string firstName, string lastName, string city, string street, string state, string zipCode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Street = street;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ Core/OrderAggregate/DeliveryMethod.cs
@@ -0,0 +1,11 @@
+namespace Core.OrderAggregate
+{
+    public class DeliveryMethod
+    {
+        public string ShortName { get; set; }
+        public string DeliveryTime { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
