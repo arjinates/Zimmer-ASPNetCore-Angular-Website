35e21a8548635416968629ad4ebeae4178d4b3aa
Arjin Ateş <arjinates@hotmail.com>
2023-06-03 01:00:08 +0300

    MappingProfiles updated OrdersController updated

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -36,17 +36,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
-            var order = await _orderService.GetOrdersForUserAsync(email);
+            var orders = await _orderService.GetOrdersForUserAsync(email);
 
-            return Ok(order);
+            return Ok(_mapper.Map<IReadOnlyList<OrderToReturnDto>>(orders));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrderById(int id)
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
@@ -54,7 +54,7 @@ namespace API.Controllers
 
             if(order == null) return NotFound(new ApiResponse(404));
 
-            return order;
+            return _mapper.Map<OrderToReturnDto>(order);
         }
 
         [HttpGet("deliveryMethods")]
