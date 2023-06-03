c0ca62b90bb6a121a46613e8e9e5d5b6fc9d92ba
Arjin Ateş <arjinates@hotmail.com>
2023-06-03 17:53:32 +0300

    order totals added to checkout page

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutRoutingModule } from './checkout-routing.module';
+import { SharedModule } from "../shared/shared.module";
 
 
 
 @NgModule({
-  declarations: [
-    CheckoutComponent
-  ],
-  imports: [
-    CommonModule,
-    CheckoutRoutingModule
-  ]
+    declarations: [
+        CheckoutComponent
+    ],
+    imports: [
+        CommonModule,
+        CheckoutRoutingModule,
+        SharedModule
+    ]
 })
 export class CheckoutModule { }
