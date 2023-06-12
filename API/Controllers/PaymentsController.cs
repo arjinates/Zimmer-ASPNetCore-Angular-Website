0fe97e421b2d651c8e5cfd051e01116469ccec57
Arjin Ateş <arjinates@hotmail.com>
2023-06-12 16:41:10 +0300

    paymentsService updated

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -45,12 +45,14 @@ namespace API.Controllers
                 case "payment_intent.succeeded":
                     intent= (PaymentIntent)stripeEvent.Data.Object;
                     _logger.LogInformation("Payment succeeded: ", intent.Id);
-                    //TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+                    ;_logger.LogInformation("Order updated to payment received: ", order.Id);
                     break;
                  case "payment_intent.payment_failed":
                     intent= (PaymentIntent)stripeEvent.Data.Object;
                     _logger.LogInformation("Payment failed: ", intent.Id);
-                    //TODO: update the order with the new status
+                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+                    _logger.LogInformation("Order updated to payment failed: ", order.Id);
                     break;   
             }
 
