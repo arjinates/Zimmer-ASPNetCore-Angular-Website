417822749e55d6c08e48be1297816c54696e005c
Arjin Ateş <arjinates@hotmail.com>
2023-06-14 19:26:53 +0300

    shopService updated-caching

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,13 +5,16 @@ import { Pagination } from '../shared/Models/pagination';
 import { Brand } from '../shared/Models/brand';
 import { Type } from '../shared/Models/type';
 import { ShopParams } from '../shared/Models/shopParams';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-  baseUrl = 'https://localhost:5001/api/'
-
+  baseUrl = 'https://localhost:5001/api/';
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
   constructor(private http: HttpClient) { }
 
   getProducts(shopParams: ShopParams) {
@@ -24,19 +27,35 @@ export class ShopService {
     params = params.append('pageSize', shopParams.pageSize);
     if(shopParams.search) params = params.append('search', shopParams.search);
     
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params}).pipe(
+      map(response => {
+        this.products = response.data;
+        return response;
+      })
+    )
   }
 
   getProduct(id: number) {
+    const product = this.products.find(p => p.id === id);
+
+    if (product) return of(product);
+
     return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
-    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+    if (this.brands.length > 0) return of(this.brands);
+
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands').pipe(
+      map(brands => this.brands = brands)
+    );
   }
 
   getTypes() {
-    return this.http.get<Type[]>(this.baseUrl + 'products/types');
-  }
+    if (this.types.length > 0) return of(this.types)
+    
+    return this.http.get<Type[]>(this.baseUrl + 'products/types').pipe(
+      map(types => this.types = types)
+    );
+ }
 }
-
